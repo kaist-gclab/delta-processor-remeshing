aba3fe0e188af142c756a21a854ce73e87e5246a
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-10-18 20:16:59 +0900

    삼각형 분할 준비

--- src/remeshingPhaseTwo.ts
+++ src/remeshingPhaseTwo.ts
@@ -1,4 +1,25 @@
+import { computeFaceAspectRatio } from './evaluation';
 import { Model } from './types';
+import { getEdges, lengthSquared } from './util';
 
-export function phaseTwo(model: Model) {
+export function phaseTwo(model: Model, targetAspectRatio: number) {
+    for (const face of model.faces) {
+        const aspectRatio = computeFaceAspectRatio(model, face);
+        if (aspectRatio >= targetAspectRatio) {
+            continue;
+        }
+
+        const { p, q, r } = getEdges(model, face);
+        const lengthSquaredP = lengthSquared(p);
+        const lengthSquaredQ = lengthSquared(q);
+        const lengthSquaredR = lengthSquared(r);
+
+        if (lengthSquaredP < lengthSquaredQ && lengthSquaredP < lengthSquaredQ) {
+            // P
+        } else if (lengthSquaredQ < lengthSquaredP && lengthSquaredQ < lengthSquaredR) {
+            // Q
+        } else {
+            // R
+        }
+    }
 }
