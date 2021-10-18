3f290f7e836e665524b40b31ce1488cebbe6fe62
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-10-18 19:00:31 +0900

    평가를 수행하는 함수 분리

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,20 @@ const OutputObjFilePath = 'output.obj';
 async function main() {
     console.log('delta-processor-remeshing');
     const model = await loadFile(InputObjFilePath);
-    console.log(`${model.vertices.length} vertices`);
-    console.log(`${model.faces.length} faces`);
 
-    const modelQuality = computeModelQuality(model);
-    const modelAverageAspectRatio = computeModelAverageAspectRatio(model);
-    const modelMinimumAngle = computeModelMinimumAngle(model);
-    console.log(`Quality ${modelQuality}`);
-    console.log(`AverageAspectRatio ${modelAverageAspectRatio}`);
-    console.log(`MinimumAngle ${modelMinimumAngle}`);
+    const evaluate = () => {
+        console.log(`${model.vertices.length} vertices`);
+        console.log(`${model.faces.length} faces`);
+        const modelQuality = computeModelQuality(model);
+        const modelAverageAspectRatio = computeModelAverageAspectRatio(model);
+        const modelMinimumAngle = computeModelMinimumAngle(model);
+        console.log(`Quality ${modelQuality}`);
+        console.log(`AverageAspectRatio ${modelAverageAspectRatio}`);
+        console.log(`MinimumAngle ${modelMinimumAngle}`);
+    };
 
+    console.log('input');
+    evaluate();
     await saveFile(OutputObjFilePath, model);
 }
 main();
