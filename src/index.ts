eda185f36bb67f3b23e2a991d0d04809273b3270
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-10-18 17:41:49 +0900

    성능 계산 명령 추가

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('delta-processor-remeshing');
+import { computeModelAverageAspectRatio, computeModelMinimumAngle, computeModelQuality } from './evaluation';
+import { loadFile } from './loader';
+
+const ObjFilePath = '';
+
+async function main() {
+    console.log('delta-processor-remeshing');
+    const model = await loadFile(ObjFilePath);
+    console.log(`${model.vertices.length} vertices`);
+    console.log(`${model.faces.length} faces`);
+
+    const modelQuality = computeModelQuality(model);
+    const modelAverageAspectRatio = computeModelAverageAspectRatio(model);
+    const modelMinimumAngle = computeModelMinimumAngle(model);
+    console.log(`Quality ${modelQuality}`);
+    console.log(`AverageAspectRatio ${modelAverageAspectRatio}`);
+    console.log(`MinimumAngle ${modelMinimumAngle}`);
+}
+main();
