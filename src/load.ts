31632fd2e5a3a313b90a3ad8e07680561351fac9
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-10-20 13:28:16 +0900

    OBJ 명령 추가 지원

--- src/load.ts
+++ src/load.ts
@@ -1,6 +1,8 @@
 import { readFile } from 'fs/promises';
 import { Face, Model, Vertex } from './types';
 
+const NotImplemented = ['vt', 'g', 'mtllib', '#', 'usemtl'];
+
 export async function loadFile(filePath: string): Promise<Model> {
     const vertices: Vertex[] = [];
     const faces: Face[] = [];
@@ -19,16 +21,28 @@ export async function loadFile(filePath: string): Promise<Model> {
                 y: Number(tokens[2]),
                 z: Number(tokens[3]),
             });
-        } else if (lineType === 'f' && tokens.length === 4) {
-            faces.push({
-                v1: Number(tokens[1]) - 1,
-                v2: Number(tokens[2]) - 1,
-                v3: Number(tokens[3]) - 1,
-            });
+        } else if (lineType === 'f') {
+            const first = parseFaceIndices(tokens[1]);
+            for (let vertexId = 3; vertexId < tokens.length; vertexId++) {
+                faces.push({
+                    v1: first - 1,
+                    v2: parseFaceIndices(tokens[vertexId - 1]) - 1,
+                    v3: parseFaceIndices(tokens[vertexId]) - 1,
+                });
+            }
+        } else if (NotImplemented.includes(lineType)) {
+            continue;
         } else {
             console.error(line);
             process.exit(-1);
         }
     }
     return { vertices, faces };
+}
+
+function parseFaceIndices(indices: string): number {
+    if (indices.includes('/')) {
+        return parseFaceIndices(indices.split('/')[0]);
+    }
+    return Number(indices);
 }
